a3e1e5bc2978cdedfa0fc924b9a8399d6d0b2aa5
Etamine <etam1ne@yandex.ru>
2023-06-29 22:58:21 +0300

    initial user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { PostController } from './post/post.controller';
+import { CommentController } from './comment/comment.controller';
+import { CommunityController } from './community/community.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    UserController,
+    PostController,
+    CommentController,
+    CommunityController,
+  ],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
